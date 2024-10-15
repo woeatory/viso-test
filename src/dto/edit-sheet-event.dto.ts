e369526afc84efda93e6ae664aefe46729a91bd8
Yurii Bobal <79090254+woeatory@users.noreply.github.com>
2024-10-15 15:35:25 +0300

    Add editSheet event webhook

---
+++ src/dto/edit-sheet-event.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class EditSheetEventDto {
+  @IsIn(['NONE', 'CUSTOM_FUNCTION', 'LIMITED', 'FULL'])
+  authMode: string;
+
+  @IsOptional()
+  @IsString()
+  oldValue: string;
+
+  @IsNumber()
+  range: number;
+
+  @IsString()
+  source: string;
+
+  @IsOptional()
+  triggerUid: any;
+
+  @IsEmail()
+  user: string;
+
+  @IsString()
+  value: string;
+}
